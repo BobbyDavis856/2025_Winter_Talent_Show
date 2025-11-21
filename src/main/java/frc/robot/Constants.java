728c56c90082bfbf075d7f81cea9210911281de0
krishB67 <krishnabhargava997@gmail.com>
2025-11-21 16:47:20 -0500

    Potential Correct Shooter Subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,11 +1,8 @@
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Meters;
-
-import edu.wpi.first.units.measure.Distance;
-
 public class Constants {
-    public final class DriveConstants {
+
+    public static final class DriveConstants {
         public static final int LEFT_MOTOR_1_ID = 10;
         public static final int LEFT_MOTOR_2_ID = 11;
         public static final boolean LEFT_MOTORS_REVERSED = false;
@@ -14,13 +11,22 @@ public class Constants {
         public static final int RIGHT_MOTOR_2_ID = 21;
         public static final boolean RIGHT_MOTORS_REVERSED = true;
 
-        public static final Distance WHEEL_BASE_WIDTH = Meters.of(1);
+        public static final double WHEEL_BASE_WIDTH = 1.0;
 
         public static final int GYRO_ID = 1;
     }
 
-    public final class ControlConstants {
-        public static double FOWARD_SPEED = 0.25;
+    public static final class ControlConstants {
+        public static double FORWARD_SPEED = 0.25;
         public static double RIGHT_SPEED = 0.25;
+    }
+
+    public static final class ShooterConstants {
+        public static final double kP = 0.0;
+        public static final double kI = 0.0;
+        public static final double kD = 0.0;
+        public static final double shooterMotorEncoderOffset =0;
+        public static final double speed=0;
+        public static int SHOOTER_MOTOR = 1;
     }
 }
--- src/main/java/frc/robot/Subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/Subsystems/ShooterSubsystem.java
@@ -1,7 +1,43 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Main;
 
 public class ShooterSubsystem extends SubsystemBase {
+
+    private final SparkMaxConfig shooterConfig = new SparkMaxConfig();
+    public final SparkMax shooterMotor = new SparkMax(Constants.ShooterConstants.SHOOTER_MOTOR,MotorType.kBrushless);
+    public final SparkClosedLoopController shooterController = shooterMotor.getClosedLoopController();
+    double kP = Constants.ShooterConstants.kP;
+    double kI = Constants.ShooterConstants.kI;
+    double kD = Constants.ShooterConstants.kD;
+    public ShooterSubsystem(){
+        shooterConfig.absoluteEncoder.zeroOffset((Constants.ShooterConstants.shooterMotorEncoderOffset));
+        shooterConfig.idleMode(IdleMode.kBrake);
+        shooterConfig.closedLoop.feedbackSensor(FeedbackSensor.kAbsoluteEncoder).p(Constants.ShooterConstants.kP).i(Constants.ShooterConstants.kI).d(Constants.ShooterConstants.kD).outputRange(-1, 1);
+        shooterMotor.configure(shooterConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    }
+     public void setSpeed(double speed) {
+        shooterController.setReference(speed, ControlType.kVelocity);
+        
+    }
     
+  
+
+
 }
